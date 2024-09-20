2644b03142d117440c71332964808d927656737a
jono1231 <jonwang2015@gmail.com>
2024-09-21 00:07:09 +0200

    getallheaders

--- Burpsuite/burp1.php
+++ Burpsuite/burp1.php
@@ -1,17 +1,5 @@
 <?php
-function getRequestHeaders() {
-    $headers = array();
-    foreach($_SERVER as $key => $value) {
-        if (substr($key, 0, 5) <> 'HTTP_') {
-            continue;
-        }
-        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
-        $headers[$header] = $value;
-    }
-    return $headers;
-}
-
-$headers = getRequestHeaders();
+$headers = getallheaders();
 
 foreach ($headers as $header => $value) {
     echo "$header: $value <br />\n";
