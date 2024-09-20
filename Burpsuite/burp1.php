8947f772914c92334608c6aca559ee1f10ddcfcd
jono1231 <jonwang2015@gmail.com>
2024-09-21 00:18:08 +0200

    test header

--- Burpsuite/burp1.php
+++ Burpsuite/burp1.php
@@ -22,10 +22,16 @@
 <body>
 
 <?php
-    $headers = getallheaders();
-    foreach ($headers as $header => $value) {
-        echo "<div class='centered-text'>$header: $value</div>";
-    }
+    // $headers = getallheaders();
+    // foreach ($headers as $header => $value) {
+        
+    // }
+    if (isset($_SERVER['HTTP_USER_AGENT'])) {
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        echo "User-Agent: " . $userAgent;
+     } else {
+        echo "User-Agent header is not set.";
+     }
 ?>
 
 </body>
