848bfe6ccf85361eb5d46e2396705acdb30897a5
jono1231 <jonwang2015@gmail.com>
2024-09-21 00:26:43 +0200

    first burpsuite challenge should be done

--- Burpsuite/burp1.php
+++ Burpsuite/burp1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Centered Text</title>
+    <title>The cat whisperers</title>
     <style>
         body, html {
             height: 100%;
@@ -22,7 +22,6 @@
 <body>
 
 <?php
-    $page -> setTitle("Nya?");
     if (isset($_SERVER['HTTP_USER_AGENT'])) {
         $userAgent = strtolower($_SERVER['HTTP_USER_AGENT']);
         if (str_contains($userAgent, "meowlingual")) {
